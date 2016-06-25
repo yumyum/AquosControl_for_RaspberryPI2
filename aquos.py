af0653965079ef528685669b9ba9252ff23bfddd
yumyum <yumyum7@gmail.com>
2016-06-25 15:30:57 +0000

    Use new fliclib.
    https://github.com/50ButtonsEach/fliclib-linux-hci

--- aquos.py
+++ aquos.py
@@ -1,5 +1,6 @@
 from os import path
-from flic import flic
+import fliclib
+from fliclib import ClickType
 import pygame.mixer
 import socket
 
@@ -11,75 +12,46 @@ COMMAND_CLICK = "IAVD0002"
 COMMAND_DOUBLE_CLICK = "IAVD0001"
 COMMAND_HOLD = "ITVD0   "
 
-client = flic.Client()
+client = fliclib.FlicClient("localhost")
 
 CUR_DIR = path.dirname(path.abspath( __file__))
 
-class ButtonEventListener(flic.ButtonEventListener):
-    def __init__(self):
-        super(ButtonEventListener, self).__init__()
-        pygame.mixer.init()
 
-    def getHash(self):
-        return "main"
-
-    def onButtonUpOrDown(self, deviceId, queued, timeDiff, isUp, isDown):
-        manager = client.getManager()
-        button = manager.getButton(deviceId)
-        #print(button.getDeviceId() + (" up" if isUp else " down"))
-        #self.aquosCtl.onButtonUpOrDown(deviceId, isUp)
-    def onButtonSingleOrDoubleClickOrHold(self, deviceId, queued, timeDiff, isSingleClick, isDoubleClick, isHold):
-        if isSingleClick:
-            self.sendCommand(COMMAND_CLICK)
-        elif isDoubleClick:
-            self.sendCommand(COMMAND_DOUBLE_CLICK)
-        elif isHold:
-            self.sendCommand(COMMAND_HOLD)
-
-    def sendCommand(self, command):
-        print("sendCommand:" + command)
-        pygame.mixer.music.load(CUR_DIR + "/test.mp3")
-        pygame.mixer.music.play(1)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((HOST, PORT))
-        sock.send(command)
-        sock.send('\r\n')
-        print(sock.recv(BUF_SIZE))
-        sock.close()
-
-buttonEventListener = ButtonEventListener()
-
-def addButtonEventListener(button):
-    button.addButtonEventListener(buttonEventListener)
-
-class ButtonListener(flic.ButtonListener):
-    def getHash(self):
-        return "main"
-
-    def onButtonDiscover(self, button):
-        addButtonEventListener(button)
-
-buttonListener = ButtonListener()
-
-class InitializedCallback(flic.CallbackVoid):
-    def callback(self):
-        print("Initialized")
-        manager = client.getManager()
-        buttons = manager.getButtons()
-        try:
-            for button in buttons:
-                addButtonEventListener(button)
-            manager.addButtonListener(buttonListener)
-        except:
-            pass
-
-class UninitializedCallback(flic.CallbackBool):
-    def callback(self):
-        print("Uninitialized")
+def sendCommand(command):
+    print("sendCommand:" + command)
+    pygame.mixer.music.load(CUR_DIR + "/test.mp3")
+    pygame.mixer.music.play(1)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    sock.send(command.encode('utf-8'))
+    sock.send('\r\n'.encode('utf-8'))
+    print(sock.recv(BUF_SIZE))
+    sock.close()
 
 
-init = InitializedCallback()
-uninit =  UninitializedCallback()
-client.start(init.getCallback(), uninit.getCallback())
+def on_button_event(click_type):
+    if click_type == ClickType.ButtonSingleClick:
+        sendCommand(COMMAND_CLICK)
+    elif click_type == ClickType.ButtonDoubleClick:
+        sendCommand(COMMAND_DOUBLE_CLICK)
+    elif click_type == ClickType.ButtonHold:
+        sendCommand(COMMAND_HOLD)
 
-client.run()
+def got_button(bd_addr):
+    cc = fliclib.ButtonConnectionChannel(bd_addr)
+    cc.on_button_single_or_double_click_or_hold = \
+        lambda channel, click_type, was_queued, time_diff: \
+            on_button_event(click_type)
+    client.add_connection_channel(cc)
+
+def got_info(items):
+    print(items)
+    for bd_addr in items["bd_addr_of_verified_buttons"]:
+        got_button(bd_addr)
+
+
+pygame.mixer.init()
+client.get_info(got_info)
+client.on_new_verified_button = got_button
+client.handle_events()
+
--- startAquos.sh
+++ startAquos.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-FLIC_PATH="/home/pi/flic"
+FLIC_PATH="/home/pi/fliclib-linux-hci/bin/"
 
 sudo hciconfig hci0 up
-sudo ${FLIC_PATH}/armv7l/daemon -d
-forever start -c python ./aquos.py
+sudo ${FLIC_PATH}/armv6l/flicd -d -f flic.sqlite3
+forever start -c python3 ./aquos.py
 amixer set PCM on 100%
